8ddd6d950b8a7a8ff5e0fa73adfdd13336cc63e3
Philip Hale <p.hale.09@aberdeen.ac.uk>
2011-03-28 20:44:00 +0100

    all test cases from p171 implemented

--- test/org/phil/diaryprototype/test/DayTest.java
+++ test/org/phil/diaryprototype/test/DayTest.java
@@ -1,83 +1,108 @@
 package org.phil.diaryprototype.test;
 
 import static org.junit.Assert.*;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.phil.diaryprototype.Appointment;
 import org.phil.diaryprototype.Day;
 
 public class DayTest {
-	//private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
-	//@Before
-	//public void setUpStreams()  {
-	//	System.setOut(new PrintStream(outContent));
-	//}
-	
-	
+		
 	@Test
 	public void testAppointmentsSize() {
-		Day testDay = new Day(1);
-		assertEquals("", Day.MAX_APPOINTMENTS_PER_DAY, testDay.getAppointmentsLength());
+		Day day = new Day(1);
+		assertEquals(Day.MAX_APPOINTMENTS_PER_DAY, day.getAppointmentsLength());
 	}
 	
 	@Test
-	public void testFindSpace() {
-		fail("Not yet implemented");
+	public void testFindSpaceEmpty() {
+		Day day = new Day(1);
+		int actual = day.findSpace(new Appointment("" , 1));
+		assertEquals(Day.START_OF_DAY, actual);
+	}
+	
+	@Test
+	public void testFindSpaceFull() {
+		Day day = new Day(1);
+		day = fillDay(day);
+		int actual = day.findSpace(new Appointment("", 1));
+		assertEquals(-1, actual);		
 	}
 
 	@Test
-	public void testMakeAppointment() {
-		fail("Not yet implemented");
+	public void testMakeAptFirst() {
+		Day day = new Day(1);
+		boolean actual = day.makeAppointment(Day.START_OF_DAY, new Appointment("Test", 1));
+		assertTrue(actual);
+		
+		/*
+		 * Need to assert that the new appointment at the start of the day 
+		 * refers to the newly created appointment. 
+		 * Appointment actual = day.getAppointment(Day.START_OF_DAY);
+		 * Appointment expected = new Appointment("Test", 1);
+		 * assertSame(expected, actual);
+		*/
+	}
+	
+	@Test
+	public void testMakeAptLast() {
+		Day day = new Day(1);
+		boolean actual = day.makeAppointment(Day.FINAL_APPOINTMENT_TIME, new Appointment("Test", 1));
+		assertTrue(actual);
+	}
+		
+	@Test 
+	public void testMakeAptLowerFail() {
+		Day day = new Day(1);
+		boolean actual = day.makeAppointment(Day.START_OF_DAY - 1, new Appointment("Test", 1));
+		assertFalse(actual);
+	}
+	
+	@Test
+	public void testMakeAptUpperFail() {
+		Day day = new Day(1);
+		boolean actual = day.makeAppointment(Day.FINAL_APPOINTMENT_TIME+1, new Appointment("Test", 1));
+		assertFalse(actual);
 	}
 
 	@Test
 	public void testShowAppointmentsEmpty() {
-		Day testDay = new Day(1);
+		Day day = new Day(1);
 		StringBuilder sb = new StringBuilder();
-		sb.append("=== Day " + testDay.getDayNumber() + " ===\n");
+		sb.append("=== Day " + day.getDayNumber() + " ===\n");
         int time = Day.START_OF_DAY;
         for(int i = 0; i < Day.MAX_APPOINTMENTS_PER_DAY; i++) {
             sb.append(time + ": \n");
             time++;
         }
-        
-		StringBuilder actual = testDay.showAppointments();
+		StringBuilder actual = day.showAppointments();
 		assertEquals(sb.toString(), actual.toString());
 	}
 	
 	@Test
 	public void testShowAppointmentsFull()  {
-		Day testDay = new Day(1);
-		for (int time = Day.START_OF_DAY;
-							time <= Day.FINAL_APPOINTMENT_TIME;
-								time++)  {
-			testDay.makeAppointment(
-					time, new Appointment("Test " + time, 1));
-		}
-		StringBuilder actual = testDay.showAppointments();
+		Day day = new Day(1);
+		day = fillDay(day);
+		StringBuilder actual = day.showAppointments();
 		
 		StringBuilder expected = new StringBuilder();
-		expected.append("=== Day " + testDay.getDayNumber() + " ===\n");
+		expected.append("=== Day " + day.getDayNumber() + " ===\n");
         int time = Day.START_OF_DAY;
         for(int i = 0; i < Day.MAX_APPOINTMENTS_PER_DAY; i++) {
             expected.append(time + ": Test " + time + "\n");
             time++;
         }
-		
-		
-		
 		assertEquals(expected.toString(), actual.toString());
 	}
 	
-	//@After
-	//public void cleanUpStreams()  {
-	//	System.setOut(null);
-	//}
+	private Day fillDay(Day day) {
+		for (int time = Day.START_OF_DAY;
+		time <= Day.FINAL_APPOINTMENT_TIME;
+		time++)  {
+			day.makeAppointment(
+					time, new Appointment("Test " + time, 1));
+		}
+		return day;
+	}
+
 
 }
